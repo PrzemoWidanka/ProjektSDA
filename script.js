e003c715110d9fa6e0e0fb2a5bbcd47fed6d5e01
przemekwidanka <przemyslaw.widanka@gmail.com>
2017-08-30 20:58:13 +0200

    Corrected header game board and added aside game board.

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ $(document).ready(function () {
     var headerGameBoard = $("#header-game-board");
     var asideGameBoard = $("#aside-game-board");
 
-    var letters = ['A', 'B', 'C', 'D', 'E', 'F','G'];
+    var letters = ['', 'A', 'B', 'C', 'D', 'E', 'F','G'];
     var numbers = [0, 1, 2, 3, 4, 5, 6];
 
     var content = [];
     var writeLetters = "";
+    var writeNumbers = "";
 
-    for (var i = 0; i < rowsCount; i++){
+    for (var i = 0; i <= rowsCount; i++){
         writeLetters = writeLetters +  '<div class="header-game-board ">' + letters[i] + '</div>';
     }
 
+    for (var i = 0; i < colsCount; i++){
+        writeNumbers = writeNumbers +  '<div class="aside-game-board ">' + numbers[i] + '</div>';
+    }
 
     for (var i = 0; i < colsCount; i++) {
         for (var j = 0; j < rowsCount; j++) {
@@ -25,5 +29,6 @@ $(document).ready(function () {
         }
     }
     headerGameBoard.html(writeLetters);
+    asideGameBoard.html(writeNumbers);
     gameBoard.html(content);
 });
