a6452fb96bd784959a07a8b9e2b896ddd25e9c22
przemekwidanka <przemyslaw.widanka@gmail.com>
2017-08-30 20:28:34 +0200

    Corrected template, added colsCount and rows Count. Created new dives header and aside.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-var square = 49;
+var colsCount = 7;
+var rowsCount = 7;
 
 $(document).ready(function () {
-    var gameboard = $("#gameboard");
+    var gameBoard = $("#game-board");
+    var headerGameBoard = $("#header-game-board");
+    var asideGameBoard = $("#aside-game-board");
+
+    var letters = ['A', 'B', 'C', 'D', 'E', 'F','G'];
+    var numbers = [0, 1, 2, 3, 4, 5, 6];
+
+    var content = [];
+    var writeLetters = "";
+
+    for (var i = 0; i < rowsCount; i++){
+        writeLetters = writeLetters +  '<div class="header-game-board ">' + letters[i] + '</div>';
+    }
+
+
+    for (var i = 0; i < colsCount; i++) {
+        for (var j = 0; j < rowsCount; j++) {
+
+            var id = i + "" + j;
+            content = content + '<div id="' + id + '">' + '</div>';
+        }
+    }
+    headerGameBoard.html(writeLetters);
+    gameBoard.html(content);
 });
-
-var letters = ['', 'A', 'B', 'C', 'D', 'E', 'F'];
-var numbers = [0, 1, 2, 3, 4, 5, 6];
-
-var content = ""
-
-for (var i = 0; i < square; i++) {
-    var id = i;
-    if (i > 0 && i < 7) {
-        content = content + '<div class="header-gameboard"  id="' + id + '">' + letters[i] + '</div>';
-    } else if (i % 7 === 0 && i > 0) {
-        content = content + '<div class="aside-gameboard" id="' + id + '">' + numbers[i / 7] + '</div>';
-    } else content = content + '<div id="' + id + '">' + '</div>';
-}
-
-gameboard.innerHTML = content;
