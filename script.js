e3c673d1d4a84873c8a0639c8eccdd6a4f00d11d
przemekwidanka <przemyslaw.widanka@gmail.com>
2017-08-31 20:19:25 +0200

    Created clicking fields checks, miss or clicks, check if target was hit.

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
+//variable columns and rows
+
 var colsCount = 7;
 var rowsCount = 7;
-var fieldCount = 49;
 
 $(document).ready(function () {
+    //access to html
     var gameBoard = $("#game-board");
     var headerGameBoard = $("#header-game-board");
     var asideGameBoard = $("#aside-game-board");
 
+    //variable with letters and numbers
     var letters = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G'];
     var numbers = [1, 2, 3, 4, 5, 6, 7];
 
+    //empty variable to assigment new html
     var content = "";
     var writeLetters = "";
     var writeNumbers = "";
 
+    //create letters
     for (var i = 0; i <= rowsCount; i++) {
         writeLetters = writeLetters + '<div class="header-game-board ">' + letters[i] + '</div>';
     }
 
+    //create numbers
     for (var i = 0; i < colsCount; i++) {
         writeNumbers = writeNumbers + '<div class="aside-game-board ">' + numbers[i] + '</div>';
     }
 
+    //create gameboard
     for (var i = 0; i < colsCount; i++) {
         for (var j = 0; j < rowsCount; j++) {
             var fieldGameBoard = [
@@ -29,12 +36,43 @@ $(document).ready(function () {
                 [j]
             ];
             var id = "field_" + i + "_" + j;
-            content = content + '<div id="' + id + '">' + fieldGameBoard + '</div>';
+            content = content + '<div id="' + id + '">' + '</div>';
             console.log(fieldGameBoard);
         }
     }
 
+    //create ships
+    var hitCount = 0;
 
+    var gameBoardWithShips = [
+        [0,0,0,1,0,0,0],
+        [0,0,1,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,0],
+        [1,0,0,0,0,0,0]
+    ]
+
+    gameBoard.click(function(e){
+
+        var coOrdinates = event.target.id.split('_');
+        // console.log(coOrdinates[1], coOrdinates[2]);
+        var shootResult = gameBoardWithShips[coOrdinates[1]][coOrdinates[2]];
+        if(shootResult === 1){
+            shootResult = 2;
+            hitCount++;  
+            e.target.style.background = "red";   
+            if(hitCount === 5){
+                setTimeout(function(){alert("WIN"); }, 10);
+            }
+        }
+        // console.log(gameBoardWithShips[coOrdinates[1]][coOrdinates[2]]);
+   
+    });
+
+        
+    //display html
     headerGameBoard.html(writeLetters);
     asideGameBoard.html(writeNumbers);
     gameBoard.html(content);
