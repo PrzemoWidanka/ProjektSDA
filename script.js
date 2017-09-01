7f239731364d9074a8d1bcf87128c9c806917d51
przemekwidanka <przemyslaw.widanka@gmail.com>
2017-09-01 20:00:08 +0200

    Ships are add random location.

--- script.js
+++ script.js
@@ -43,22 +43,45 @@ $(document).ready(function () {
 
     //create ships
     var hitCount = 0;
+    var col, row;
+    var numberShips = 5;
 
     var gameBoardWithShips = [
-        [0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [1, 0, 0, 0, 0, 0, 0]
-    ]
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    function insertShip() {
+
+        row = Math.floor(Math.random() * rowsCount);
+        col = Math.floor(Math.random() * colsCount);
+        if (gameBoardWithShips[row][col] !== 1) {
+
+            gameBoardWithShips[row][col] = 1;
+        } else {
+            insertShip()
+        }
+    }
+
+    for (var i = 0; i < numberShips; i++) {
+        insertShip();
+    }
 
     //function that checks fields(empty or ship)
     gameBoard.click(function (e) {
 
         var coOrdinates = e.target.id.split('_');
+
+        if (coOrdinates.length < 3) {
+            return;
+        }
         var shotResult = gameBoardWithShips[coOrdinates[1]][coOrdinates[2]];
+
         var target = $(e.target);
         if (shotResult === 1) {
             target.removeClass("game-board-div");
