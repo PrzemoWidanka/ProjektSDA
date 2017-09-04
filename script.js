e69d8bae4b1ed4acfffe82c2ae4eb3a89d913152
przemekwidanka <przemyslaw.widanka@gmail.com>
2017-09-04 20:48:35 +0200

    Restart game without reload

--- script.js
+++ script.js
@@ -19,46 +19,84 @@ $(document).ready(function () {
     var numbers = [1, 2, 3, 4, 5, 6, 7];
 
     //empty variables to assigment new content html
-    var content = "";
-    var writeLetters = "";
-    var writeNumbers = "";
 
-    //create letters 
-    for (var i = 0; i <= rowsCount; i++) {
-        writeLetters = writeLetters + '<div class="header-game-board ">' + letters[i] + '</div>';
-    }
+    function createLetters() {
 
-    //create numbers
-    for (var i = 0; i < colsCount; i++) {
-        writeNumbers = writeNumbers + '<div class="aside-game-board ">' + numbers[i] + '</div>';
-    }
+        var writeLetters = "";
 
-    //create game board
-    for (var i = 0; i < colsCount; i++) {
-        for (var j = 0; j < rowsCount; j++) {
-            var fieldGameBoard = [
-                [i],
-                [j]
-            ];
-            var id = "field_" + i + "_" + j;
-            content = content + '<div id="' + id + '" class="game-board-div">' + '</div>';
+        for (var i = 0; i <= rowsCount; i++) {
+            writeLetters = writeLetters + '<div class="header-game-board ">' + letters[i] + '</div>';
         }
+        return writeLetters;
+    }
+
+    function createNumbers() {
+
+        var writeNumbers = "";
+
+        for (var i = 0; i < colsCount; i++) {
+            writeNumbers = writeNumbers + '<div class="aside-game-board ">' + numbers[i] + '</div>';
+        }
+        return writeNumbers;
+    }
+
+    function createGameBoard() {
+        var content = "";
+        for (var i = 0; i < colsCount; i++) {
+            for (var j = 0; j < rowsCount; j++) {
+                var fieldGameBoard = [
+                    [i],
+                    [j]
+                ];
+                var id = "field_" + i + "_" + j;
+                content = content + '<div id="' + id + '" class="game-board-div">' + '</div>';
+            }
+        }
+        return content;
+    }
+
+    function startGame() {
+        clearGameBoard();
+        instertShips();
+        restartTime();
+        restartHit();
+        startGameTimer();
+        gameBoard.off('click');
+        gameBoard.click(asd);
+        isPlaying = true;
+        headerGameBoard.html(createLetters());
+        asideGameBoard.html(createNumbers());
+        gameBoard.html(createGameBoard());
     }
 
     //create ships
-    var hitCount = 0;
+    var hitCount;
     var numberShips = 5;
     var intervalId;
+    var time;
+    var gameBoardWithShips;
+    var isPlaying = false;
 
-    var gameBoardWithShips = [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]
-    ];
+    function restartHit() {
+        hitCount = 0;
+    }
+
+    function restartTime() {
+        time = 30;
+        $(".time").text(time);
+    }
+
+    function clearGameBoard() {
+        gameBoardWithShips = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ];
+    }
 
     function insertShip() {
 
@@ -72,13 +110,60 @@ $(document).ready(function () {
         }
     }
 
-    for (var i = 0; i < numberShips; i++) {
-        insertShip();
+    function instertShips() {
+        for (var i = 0; i < numberShips; i++) {
+            insertShip();
+        }
+    }
+
+    function startGameTimer() {
+        clearInterval(intervalId);
+        intervalId = setInterval(function () {
+            time--;
+            console.log(time);
+            if (time <= 30) {
+                $(".time").text(time);
+                buttonPlay.off('click');
+                buttonPlay.css('cursor', 'default');
+            }
+            if (time === 0) {
+                setTimeout(function () {
+                    sweetAlert({
+                        title: "Oops...",
+                        text: "TimeOUT!",
+                        type: "error",
+                        closeOnCancel: false
+                    });
+                }, 10);
+                gameBoard.off('click')
+                clearInterval(intervalId);
+            }
+        }, 1000);
     }
 
     //function that checks fields(empty or ship)
-    gameBoard.click(function (e) {
 
+    
+
+
+    //function that starts the game and starts the stopwatch
+    buttonPlay.click(function () {
+        $(".first-board").remove();
+
+        startGame();
+    });
+
+    //function that reload the website
+    buttonRestart.click(function () {
+        // location.reload();
+        startGame();
+    });
+
+    //display html
+    // startGame();
+
+    function asd(e) {
+        
         var coordinates = e.target.id.split('_');
 
         if (coordinates.length < 3) {
@@ -113,41 +198,5 @@ $(document).ready(function () {
             // container.html("Tak jest brawo TY <br><br>"+'<span onclick="location.reload()">Jeszcze raz???</span>');
         }
 
-    });
-
-    //function that starts the game and starts the stopwatch
-    buttonPlay.click(function () {
-        $(".first-board").remove();
-        var time = 0;
-        intervalId = setInterval(function () { //zmienna globalna jak inaczej?
-            time++;
-            if (time <= 30) {
-                $(".time").text(time);
-                buttonPlay.off('click');
-                buttonPlay.css('cursor','default');
-            }
-            if (time === 30) {
-                setTimeout(function () {
-                    sweetAlert({
-                        title: "Oops...",
-                        text: "TimeOUT!",
-                        type: "error",
-                        closeOnCancel: false
-                    });
-                }, 10);
-                gameBoard.off('click')
-                clearInterval(intervalId);
-            }
-        }, 1000);
-    });
-
-    //function that reload the website
-    buttonRestart.click(function(){
-        location.reload()
-    });
-
-    //display html
-    headerGameBoard.html(writeLetters);
-    asideGameBoard.html(writeNumbers);
-    gameBoard.html(content);
+    }
 });
